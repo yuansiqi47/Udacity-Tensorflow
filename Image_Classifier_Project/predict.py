34dfbba4f25c98f291bcefd70bd3b2b44ee36b73
yuansiqi47 <ganxiebaba@gmail.com>
2022-06-21 09:25:42 +0200

    making changes based on the feedback

--- Image_Classifier_Project/predict.py
+++ Image_Classifier_Project/predict.py
@@ -1,84 +1,43 @@
-import sys 
-import time 
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_datasets as tfds
 from PIL import Image
-import argparse
 import json
+import argparse
 
-batch_size = 32
-image_size = 224
+batch_size = 64
+
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+with open(args.category_names, 'r') as f:
+    class_names = json.load(f)
 
 
-class_names = {}
-
-def process_image(image): 
-   
+def process_image(image):
     image = tf.cast(image, tf.float32)
-    image= tf.image.resize(image, (image_size, image_size))
+    image = tf.image.resize(image, (224, 224))
     image /= 255
     image = image.numpy()
-    
+
     return image
-    
 
-def predict(image_path, model, top_k=5):
-    
+
+def predict(image_path, model):
     image = Image.open(image_path)
-    image = np.asarray(image)
-    image = np.expand_dims(image,  axis=0)
+    image = np.array(image)
+    image = np.expand_dims(image, axis=0)
     image = process_image(image)
-    prob_list = model.predict(image)
-    
-    
-    classes = []
-    probs = []
-    
-    rank = prob_list[0].argsort()[::-1]
-    
-    for i in range(top_k):
-        
-        index = rank[i] + 1
-        cls = class_names[str(index)]
-        
-        probs.append(prob_list[0][index])
-        classes.append(cls)
-    
-    return probs, classes
+    probs = model.predict(image)
+
+    prob = max(probs)
+    index = probs.index(prob)
+    cls = class_names[str(index)]
+
+    return prob, cls
 
 
-if __name__ == '__main__':
-    print('predict.py, running')
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('arg1')
-    parser.add_argument('arg2')
-    parser.add_argument('--top_k')
-    parser.add_argument('--category_names') 
-    
-    
-    args = parser.parse_args()
-    print(args)
-    
-    print('arg1:', args.arg1)
-    print('arg2:', args.arg2)
-    print('top_k:', args.top_k)
-    print('category_names:', args.category_names)
-    
-    image_path = args.arg1
-    
-    model = tf.keras.models.load_model(args.arg2 ,custom_objects={'KerasLayer':hub.KerasLayer} )
-    top_k = args.top_k
-    if top_k is None: 
-        top_k = 5
+# Carry out prediction
+prob, cls = predict(image_path, model)
 
-    with open(args.category_names, 'r') as f:
-        class_names = json.load(f)
-   
-    probs, classes = predict(image_path, model, top_k)
-    
-    print(probs)
-    print(classes)
+# Print probabilities and predicted classes
+print(prob)
+print(cls)
