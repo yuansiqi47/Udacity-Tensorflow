8d539a18e79ffccdbeae5eb37a18878f19bd3999
yuansiqi47 <ganxiebaba@gmail.com>
2022-06-21 11:47:20 +0200

    making changes based on the feedback

--- Image_Classifier_Project/predict.py
+++ Image_Classifier_Project/predict.py
@@ -8,50 +8,45 @@ import tensorflow_hub as hub
 
 batch_size = 64
 
+# setting
 parser = argparse.ArgumentParser()
 parser.add_argument('--image_path', action='store',
                     default='./test_images/cautleya_spicata.jpg',
                     help='Enter path to image.')
 
-parser.add_argument('--save_dir', action='store',
-                    dest='save_directory', default='my_model.h5',
+parser.add_argument('--model_dir', action='store',
+                    dest='model_dir', default='my_model.h5',
                     help='Enter location to save checkpoint in.')
 
 args = parser.parse_args()
-with open('label_map.json', 'r') as f:
-    class_names = json.load(f)
 
-
-def process_image(image):
-    image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, (224, 224))
-    image /= 255
-    image = image.numpy()
-
-    return image
-
-
-def predict(image_path, model):
-    image = Image.open(image_path)
-    image = np.array(image)
-    image = np.expand_dims(image, axis=0)
-    image = process_image(image)
-    probs = model.predict(image).flatten().tolist()
-
-    prob = max(probs)
-    index = probs.index(prob) + 1
-    cls = class_names[str(index)]
-
-    return prob, cls
-
-
-save_dir = args.save_directory
+# load the model
+save_dir = args.model_dir
 model = tf.keras.experimental.load_from_saved_model(
     save_dir, custom_objects={'KerasLayer': hub.KerasLayer})
 
-# Carry out prediction
-prob, cls = predict(args.image_path, model)
+# load the image
+image = Image.open(args.image_path)
 
-# Print probabilities and predicted classes
+# process the image
+image = np.array(image)
+image = tf.cast(image, tf.float32)
+image = tf.image.resize(image, (224, 224))
+image /= 255
+image = np.expand_dims(image, axis=0)
+
+# make prediction
+probs = model.predict(image).flatten().tolist()
+
+# open class_names file
+with open('label_map.json', 'r') as f:
+    class_names = json.load(f)
+
+# Get the probability and the predicted class
+prob = max(probs)
+index = probs.index(prob) + 1
+cls = class_names[str(index)]
+
+# Print the probability and the predicted class
 print(prob)
 print(cls)
